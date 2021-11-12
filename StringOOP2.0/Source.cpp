c823a3cba1f6532f86683981f0ce766ce60aedca
BuildTools <unconfigured@null.spigotmc.org>
2021-11-12 21:30:55 +0500

    Header_initialization

--- StringOOP2.0/Source.cpp
+++ StringOOP2.0/Source.cpp
@@ -21,36 +21,25 @@ public:
 	{	return size;}
 
 	//Constructor
-	String(int size = 80)
+	explicit String(int size = 80):size(size), str(new char[size]{})
 	{
-		this->size = size;
-		this->str = new char[size] {};
 		cout << "DefaultConstructor:\t" << this << endl;
 	}
-	String(const char* str)
+	String(const char* str):size(strlen(str)+1), str(new char[size]{})
 	{
-		this->size = strlen(str) + 1;
-		this->str = new char[size] {};
-		for (int i = 0; i < size; i++)
-		{
-			this->str[i] = str[i];
-		}
+		for (int i = 0; i < size; i++)this->str[i] = str[i];
 		cout << "Constructor:\t\t" << this << endl;
 	}
-	String(const String& other)
+	String(const String& other):size(other.size), str(new char[size] {})
 	{
-		this->size = other.size;
-		this->str = new char[size] {};
 		for (int i = 0; i < size; i++)
 		{
 			this->str[i] = other.str[i];
 		}
 		cout << "CopyConstructor:\t" << this << endl;
 	}
-	String(String&& other)
+	String(String&& other):size(other.size), str(other.str)
 	{
-		this->size = other.size;
-		this->str = other.str;
 		cout << "MoveConstructor:\t" << this << endl;
 		other.str = nullptr; other.size = 0;
 	}
@@ -99,7 +88,7 @@ public:
 
 String operator+(const String& left, const String& right)
 {
-	String buffer = left.get_size() + right.get_size() - 1;
+	String buffer(left.get_size() + right.get_size() - 1);
 	for (int i = 0; i < left.get_size(); i++)
 	{	
 		/*buffer.get_str()[i] = left.get_str()[i];*/
@@ -121,6 +110,7 @@ ostream& operator<<(ostream& os, const String& obj)
 
 
 //#define CONSTRUCTOR_CHECK
+//#define OPERATOR_PLUS_CHECK
 
 void main()
 {
@@ -128,24 +118,48 @@ void main()
 #ifdef CONSTRUCTOR_CHECK
 	String Str;
 	Str.print();
+
 	String str1 = "Hello";
 	str1.print();
 	cout << str1 << endl;
+
 	String str2 = str1;
 	cout << str2 << endl;
+
 	String str3;
 	str3 = str2;
 	cout << str3 << endl;
 #endif // CONSTRUCTOR_CHECK
 
-	/*String str1 = "Hello";
-	str1 = str1;
-	cout << str1 << endl;*/
-
+#ifdef OPERATOR_PLUS_CHECK
 	String str1 = "Hello";
 	String str2 = "World";
 	String str3 = str1 + str2;
 	cout << str3 << endl << endl;
 	str1 += str2;
 	cout << str1 << endl << endl;
-}
+#endif // OPERATOR_PLUS_CHECK
+
+	String str1(25);	//Default constructor
+	str1.print();
+
+	String str2 = "Hello";	//Single Argument
+	cout << str2 << endl;
+	str2.print();
+
+	String str3("World");	//Single Argument
+	cout << str3 << endl;
+
+	String str4();	//ќбъ€вление функции str4, котора€ ничего не принимает и возвращает объект класса String;
+
+	String str5{};	//явный вызов конструктора по умолчанию.
+	cout << str5 << endl;
+
+	String str6{ "Single arg constructor" };
+	cout << str6 << endl;
+
+	String str7 = str6;
+	cout << str7 << endl;
+
+	cout << str2 + " " + str3 << endl;
+} 
