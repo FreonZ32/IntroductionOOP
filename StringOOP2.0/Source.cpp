19c03a194df9ecd8ea82867a0400355ca18ddc70
BuildTools <unconfigured@null.spigotmc.org>
2021-11-19 22:04:15 +0500

    Metods out of class

--- StringOOP2.0/Source.cpp
+++ StringOOP2.0/Source.cpp
@@ -1,10 +1,11 @@
 #include<iostream>
 #include<string.h>
-using namespace std;
+//using namespace std;
 using std::cout;
 using std::cin;
 using std::endl;
 
+//////////////////Class declaration/////////////////////
 class String;
 String operator+(const String& left, const String& right);
 
@@ -13,74 +14,106 @@ class String
 	int size;
 	char* str;
 public:
-	const char* get_str()const
-	{	return str;	}
-	char* get_str()
-	{	return str;	}
-	int get_size()const
-	{	return size;}
+	const char* get_str()const;
+	char* get_str();
+	int get_size()const;
 
 	//Constructor
-	explicit String(int size = 80):size(size), str(new char[size]{})
-	{
-		cout << "DefaultConstructor:\t" << this << endl;
-	}
-	String(const char* str):String(strlen(str)+1)//Делигируем выделение памяти конструктору по умолчанию
-	{
-		for (int i = 0; i < size; i++)this->str[i] = str[i];
-		cout << "Constructor:\t\t" << this << endl;
-	}
-	String(const String& other):String(other.str)
-	{
-		cout << "CopyConstructor:\t" << this << endl;
-	}
-	String(String&& other):size(other.size), str(other.str)
-	{
-		cout << "MoveConstructor:\t" << this << endl;
-		other.str = nullptr; other.size = 0;
-	}
-	~String()
-	{
-		delete[] this->str;
-		cout << "Destructor:\t" << this << endl;
-	}
+	explicit String(int size = 80);
+	String(const char* str);
+	String(const String& other);
+	String(String&& other);
+	~String();
 
 	//Methods
-	void print()const
-	{
-		cout << "Size = " << size << endl;
-		cout << "Str = " << str << endl;
-	}
+	void print()const;
 	//Operator
-	String& operator=(const String& other)
-	{
-		if (this != &other)
-		{
-			delete[]this->str;
-			this->size = other.size;
-			this->str = new char[size] {};
-			for (int i = 0; i < size; i++)
-				this->str[i] = other.str[i];
-		}
-		cout << "CopyAssigment:\t\t" << this << endl;
-		return *this;
-	}
-	String& operator=(String&& other)
-	{
-		this->size = other.size;
-		this->str = other.str;
-		other.str = nullptr;
-		other.size = 0;
-		return *this;
-	}
-	String operator+=(const String& other)
-	{	return *this = *this + other;	}
-	const char& operator[](int i)const
-	{	return str[i];}
-	char& operator[](int i)
-	{	return str[i];}
-	
+	String& operator=(const String& other);
+	String& operator=(String&& other);
+	String operator+=(const String& other);
+	const char& operator[](int i)const;
+	char& operator[](int i);
 };
+
+////////////////////Class defenition/////////////////////////
+const char* String::get_str()const
+{
+	return str;
+}
+char* String::get_str()
+{
+	return str;
+}
+int String::get_size()const
+{
+	return size;
+}
+
+//Constructor
+String::String(int size) :size(size), str(new char[size] {})
+{
+	cout << "DefaultConstructor:\t" << this << endl;
+}
+String::String(const char* str) :String(strlen(str) + 1)//Делигируем выделение памяти конструктору по умолчанию
+{
+	for (int i = 0; i < size; i++)this->str[i] = str[i];
+	cout << "Constructor:\t\t" << this << endl;
+}
+String::String(const String & other) : String(other.str)
+{
+	cout << "CopyConstructor:\t" << this << endl;
+}
+String::String(String&& other) :size(other.size), str(other.str)
+{
+	cout << "MoveConstructor:\t" << this << endl;
+	other.str = nullptr; other.size = 0;
+}
+String::~String()
+{
+	delete[] this->str;
+	cout << "Destructor:\t" << this << endl;
+}
+
+//Methods
+void String::print()const
+{
+	cout << "Size = " << size << endl;
+	cout << "Str = " << str << endl;
+}
+//Operator
+String& String::operator=(const String& other)
+{
+	if (this != &other)
+	{
+		delete[]this->str;
+		this->size = other.size;
+		this->str = new char[size] {};
+		for (int i = 0; i < size; i++)
+			this->str[i] = other.str[i];
+	}
+	cout << "CopyAssigment:\t\t" << this << endl;
+	return *this;
+}
+String& String::operator=(String&& other)
+{
+	this->size = other.size;
+	this->str = other.str;
+	other.str = nullptr;
+	other.size = 0;
+	return *this;
+}
+String String::operator+=(const String& other)
+{
+	return *this = *this + other;
+}
+const char& String::operator[](int i)const
+{
+	return str[i];
+}
+char& String::operator[](int i)
+{
+	return str[i];
+}
 
 String operator+(const String& left, const String& right)
 {
@@ -99,7 +132,7 @@ String operator+(const String& left, const String& right)
 }
 
 
-ostream& operator<<(ostream& os, const String& obj)
+std::ostream& operator<<(std::ostream& os, const String& obj)
 {
 	return os << obj.get_str();
 }
